83cbecdaaaab05766a4cf45dfc602ba2e187200b
Garth Williams <garth@wi.llia.ms>
2018-03-13 13:17:52 +0100

    Remove return from memo types

--- client-web/src/config.js
+++ client-web/src/config.js
@@ -3,7 +3,6 @@ export default {
   memoTypes: [
     { value: 'MEMO_TEXT', label: 'Text', description: 'message to include in the transaction' },
     { value: 'MEMO_ID', label: 'ID', description: 'for the recipient to identify whoever is sending the lumens' },
-    { value: 'MEMO_HASH', label: 'Hash', description: 'reference to another transaction' },
-    { value: 'MEMO_RETURN', label: 'Return', description: 'reference to transaction when refunding that transaction' }
+    { value: 'MEMO_HASH', label: 'Hash', description: 'reference to another transaction' }
   ]
 }
