93807aa8b9342f4f1aa8c801ab1f397372972444
Garth Williams <garth@wi.llia.ms>
2018-03-13 10:40:58 +0100

    Add DB_HOST env var

--- server/src/config.js
+++ server/src/config.js
@@ -13,6 +13,7 @@ module.exports = {
     database: process.env.DB_NAME || 'lumenbox',
     user: process.env.DB_USER || '',
     password: process.env.DB_PASSWORD || '',
+    host: process.env.DB_HOST || 'localhost',
     port: 5432,
     ssl: false,
     max: 20, // set pool max size to 20
