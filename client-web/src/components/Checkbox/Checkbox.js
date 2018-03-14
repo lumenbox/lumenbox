731ba5c2ae69c0cc9690c30ddae07ff5ccf3eec8
Garth Williams <garth@wi.llia.ms>
2018-03-14 15:46:44 +0100

    Fixed checkbox field alignment

--- client-web/src/components/Checkbox/Checkbox.js
+++ client-web/src/components/Checkbox/Checkbox.js
@@ -1,24 +1,29 @@
 import * as React from 'react'
 
 const Checkbox = ({ label, value, name, isPristine, isValid, errorMessage, onChange }) => (
-  <div className="field">
-    <div className="control">
-      <label className="checkbox">
-        <input
-          id={`cb-${name}`}
-          className="is-checkradio"
-          type="checkbox"
-          checked={value}
-          onChange={
-            /* istanbul ignore next */ e => {
-              onChange && onChange(e.target.checked, e)
-            }
-          }
-        />{' '}
-        {label}
-      </label>
+  <div className="field is-horizontal">
+    <div className="field-label" />
+    <div className="field-body">
+      <div className="field">
+        <div className="control">
+          <label className="checkbox">
+            <input
+              id={`cb-${name}`}
+              className="is-checkradio"
+              type="checkbox"
+              checked={value}
+              onChange={
+                /* istanbul ignore next */ e => {
+                  onChange && onChange(e.target.checked, e)
+                }
+              }
+            />{' '}
+            {label}
+          </label>
+        </div>
+        {errorMessage && <p className="help is-danger">{errorMessage}</p>}
+      </div>
     </div>
-    {errorMessage && <p className="help is-danger">{errorMessage}</p>}
   </div>
 )
 
