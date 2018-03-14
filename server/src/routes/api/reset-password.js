af05ed422556b952acb58a28e507982a50c7529b
Garth Williams <garth@wi.llia.ms>
2018-03-14 16:09:49 +0100

    Fix 401 being sent too early

--- server/src/routes/api/reset-password.js
+++ server/src/routes/api/reset-password.js
@@ -9,7 +9,7 @@ const config = require('../../config')
 const rateLimit = new RateLimit(config.rateLimit.resetPassword)
 
 module.exports = ({ app, pool, config, authorise }) => {
-  app.post('/api/reset-password-request', rateLimit, (req, res) => {
+  app.post('/api/reset-password', rateLimit, (req, res) => {
     const { email, token, newPassword } = req.body
     if (!email || !token || !newPassword) {
       return res.sendStatus(401)
@@ -19,30 +19,33 @@ module.exports = ({ app, pool, config, authorise }) => {
         console.error('failed to reset password', err)
         return res.status(500).send({ error: 'Unxpected Error' })
       }
-      if (result.rows.length > 0) {
-        // accept any token generated today or yesterday so long as the user has not been updated
-        const user = changeCase.camelCase(result.rows[0])
-        const today = new Date().toDateString()
-        const yesterday = (d => d.setDate(d.getDate() - 1))(new Date()).toDateString()
-        if (
-          token === hash({ ...user, passwordResetTokenIssued: today }) ||
-          token === hash({ ...user, passwordResetTokenIssued: yesterday })
-        ) {
-          pool.query(
-            'update "user" set password_hash = $2, updated_at = now() where id = $1',
-            [user.id, generateHash(newPassword)],
-            (err, result) => {
-              if (err) {
-                console.error('failed to reset password', err)
-                return res.status(500).send({ error: 'Unxpected Error' })
-              }
-              sendMail(user, passwordChanged)
-              res.sendStatus(200)
-            }
-          )
-        }
+      if (result.rows.length === 0) {
+        res.sendStatus(401)
       }
-      res.sendStatus(401)
+      // accept any token generated today or yesterday so long as the user has not been updated
+      const user = changeCase.camelCase(result.rows[0])
+      const today = new Date().toDateString()
+      const yesterday = (d => {
+        d.setDate(d.getDate() - 1)
+        return d
+      })(new Date()).toDateString()
+      if (
+        token === hash({ ...user, passwordResetTokenIssued: today }) ||
+        token === hash({ ...user, passwordResetTokenIssued: yesterday })
+      ) {
+        pool.query(
+          'update "user" set password_hash = $2, updated_at = now() where id = $1',
+          [user.id, generateHash(newPassword)],
+          (err, result) => {
+            if (err) {
+              console.error('failed to reset password', err)
+              return res.status(500).send({ error: 'Unxpected Error' })
+            }
+            sendMail(user, passwordChanged)
+            return res.sendStatus(200)
+          }
+        )
+      }
     })
   })
 }
