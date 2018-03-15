883591523343508f4645ff1c3c36ab8b1efdbc23
Garth Williams <garth@wi.llia.ms>
2018-03-15 10:58:39 +0100

    Only render dialogs when they are open

--- client-web/src/components/Dialog/Dialog.js
+++ client-web/src/components/Dialog/Dialog.js
@@ -6,33 +6,36 @@ const Footer = styled.footer`
   justify-content: flex-end;
 `
 
-const Dialog = ({ isOpen, onClose, onOk, title, okLabel = 'OK', okClassName, cancelLabel, children }) => (
-  <div className={classNames('modal', { 'is-active': isOpen })}>
-    <div className="modal-background" onClick={onClose} />
-    <div className="modal-card">
-      {title && (
-        <header className="modal-card-head">
-          <p className="modal-card-title">{title}</p>
-          <button className="delete" aria-label="close" onClick={onClose} />
-        </header>
-      )}
-      <section className="modal-card-body">{children}</section>
-      {(onOk || cancelLabel) && (
-        <Footer className="modal-card-foot">
-          {cancelLabel && (
-            <button className="button" onClick={onClose}>
-              {cancelLabel}
-            </button>
-          )}
-          {onOk && (
-            <button className={classNames('button', 'is-success', okClassName)} onClick={onOk}>
-              {okLabel}
-            </button>
-          )}
-        </Footer>
-      )}
+const Dialog = ({ isOpen, onClose, onOk, title, okLabel = 'OK', okClassName, cancelLabel, children }) =>
+  isOpen ? (
+    <div className="modal is-active">
+      <div className="modal-background" onClick={onClose} />
+      <div className="modal-card">
+        {title && (
+          <header className="modal-card-head">
+            <p className="modal-card-title">{title}</p>
+            <button className="delete" aria-label="close" onClick={onClose} />
+          </header>
+        )}
+        <section className="modal-card-body">{children}</section>
+        {(onOk || cancelLabel) && (
+          <Footer className="modal-card-foot">
+            {cancelLabel && (
+              <button className="button" onClick={onClose}>
+                {cancelLabel}
+              </button>
+            )}
+            {onOk && (
+              <button className={classNames('button', 'is-success', okClassName)} onClick={onOk}>
+                {okLabel}
+              </button>
+            )}
+          </Footer>
+        )}
+      </div>
     </div>
-  </div>
-)
+  ) : (
+    <span />
+  )
 
 export default Dialog
